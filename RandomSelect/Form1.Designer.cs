84507ec1a54359145446911def1a90688ff62541
megadox <dhappymean@gmail.com>
2024-09-13 17:58:30 +0900

    개인 프로젝트

---
+++ RandomSelect/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace RandomSelect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRandomNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRandomCount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Random Number";
+            // 
+            // txtRandomNumber
+            // 
+            this.txtRandomNumber.Location = new System.Drawing.Point(174, 40);
+            this.txtRandomNumber.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtRandomNumber.Name = "txtRandomNumber";
+            this.txtRandomNumber.Size = new System.Drawing.Size(401, 27);
+            this.txtRandomNumber.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 137);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(177, 132);
+            this.txtResult.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(401, 27);
+            this.txtResult.TabIndex = 3;
+            // 
+            // btnCal
+            // 
+            this.btnCal.Location = new System.Drawing.Point(612, 43);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(130, 55);
+            this.btnCal.TabIndex = 4;
+            this.btnCal.Text = "Lotto";
+            this.btnCal.UseVisualStyleBackColor = true;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(612, 132);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(130, 33);
+            this.btnclear.TabIndex = 5;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 91);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Random Count";
+            // 
+            // txtRandomCount
+            // 
+            this.txtRandomCount.Location = new System.Drawing.Point(174, 88);
+            this.txtRandomCount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtRandomCount.Name = "txtRandomCount";
+            this.txtRandomCount.Size = new System.Drawing.Size(95, 27);
+            this.txtRandomCount.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 215);
+            this.Controls.Add(this.txtRandomCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRandomNumber);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRandomNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRandomCount;
+    }
+}
+
---
+++ RandomSelect/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomSelect
+{
+    public partial class Form1 : Form
+    {
+        List<string> list;
+        public Form1()
+        {
+            InitializeComponent();
+
+            list = new List<string>();
+
+            this.btnCal.Click += BtnCal_Click;
+            btnclear.Click += Btnclear_Click;
+            txtRandomCount.Text = "0";
+        }
+
+        private void Btnclear_Click(object sender, EventArgs e)
+        {
+            txtResult.Clear();
+            txtRandomNumber.Clear();
+            results = new List<int>();
+        }
+
+        List<int> results = new List<int>();
+
+        private void BtnCal_Click(object sender, EventArgs e)
+        {
+            string humanPredict = txtRandomNumber.Text;
+            int randomCount = Convert.ToInt32(txtRandomCount.Text);
+
+            if(string.IsNullOrEmpty(humanPredict) )
+            {
+                int i = 0;
+
+                while(i < 6)
+                {
+
+                    Random ran = new Random();
+                    int rInt = ran.Next(1, 45);
+                    if(!results.Contains(rInt))
+                    {
+                        results.Add(rInt);
+                        i++;
+                    }
+                    
+                }
+                
+            }
+            else
+            {
+                string[] hp = txtRandomNumber.Text.Split(' ');
+                int i = 0;
+                List<string> list = new List<string>();
+                list.AddRange(hp);
+                if(randomCount > 0)
+                {
+                    while (i <= randomCount)
+                    {
+
+                        Random ran = new Random();
+                        int rInt = ran.Next(1, 45);
+                        if(!list.Contains(rInt.ToString()))
+                        {
+                            list.Add(rInt.ToString());
+                            i++;
+                        }
+                    }
+                }
+
+                i = 0;
+                while (i < 6)
+                {
+                    Random ran = new Random();
+                    int rInt = ran.Next(0, list.Count-1);
+                    string sel = list[rInt];
+                    if(int.TryParse(sel, out int rr))
+                    {
+                        if (!results.Contains(rr))
+                    {
+                            results.Add(rr);
+                            i++;
+                        }
+                    }
+                }
+            }
+
+            results.Sort();
+
+            StringBuilder sb = new StringBuilder();
+            foreach(int i in results)
+            {
+                sb.Append($"{i}, ");
+            }
+
+            txtResult.Text = sb.ToString();
+        }
+    }
+}
